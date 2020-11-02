5ee8ece8dab53062272b29ecdcd6bdd055cdc5b6
Brian Leip <brianleip@gmail.com>
2020-11-02 11:53:36 -0800

    tested out graphql query on local wp site page.  all working nice

--- portfoliosite/src/pages/index.js
+++ portfoliosite/src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import { Link } from "gatsby"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
+import { graphql, StaticQuery } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    {/* <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -15,7 +15,34 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+
+    <StaticQuery 
+      query={ graphql `
+        {
+          allWordpressPage {
+            edges {
+              node{
+                title
+                content
+              }
+            }
+          }
+        }
+      `}
+      render={ props => (
+        <div>
+          { props.allWordpressPage.edges.map(page => (
+            <div key={page.node.id}>
+              <h1>
+                {page.node.title}
+              </h1>
+              <div dangerouslySetInnerHTML={{__html: page.node.content}} />
+            </div>
+          ))}
+        </div>
+      )} 
+    />
   </Layout>
 )
 
